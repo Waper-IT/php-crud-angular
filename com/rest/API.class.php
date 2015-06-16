b598ab014a43c4aaf8bee3cfeec81629cfedfd1e
sminutoli <sminutoli.it@gmail.com>
2015-06-16 01:20:42 -0300

    Fix typo on CORS

--- com/rest/API.class.php
+++ com/rest/API.class.php
@@ -40,7 +40,7 @@
          * Allow for CORS, assemble and pre-process the data
          */
         public function __construct($request) {
-            header("Access-Control-Allow-Orgin: *");
+            header("Access-Control-Allow-Origin: *");
             header("Access-Control-Allow-Methods: *");
             header("Content-Type: application/json");
 
